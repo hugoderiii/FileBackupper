38475a336e96373a29b947b4505488d1f705b9ee
hugoderiii <hugoderiii@gmail.com>
2017-12-15 18:20:25 +0100

    refactor client

--- client/client.py
+++ client/client.py
@@ -29,33 +29,38 @@ def Main():
 		openConnection (host, port, directory)
 
 def openConnection (host, port, root):
-	os.chdir(root)
 	sock = socket.socket()
 	sock.connect((host, port))
-	fileDictionary = {}
-	for path, subdirs, files in os.walk("."):
-		for name in files:
-			fullFileName = os.path.join(path, name)
-			if os.path.isfile(fullFileName):
-				fileDictionary[fullFileName] = os.path.getsize(fullFileName)
+	fileDictionary = getFileDictionary (root)
 
-	#send the password
+	sendPassword (sock, password)
+	sendBaseFolder (sock,root)
+	sendDictionary (sock, fileDictionary)
+	sendFiles(sock)
+
+	print ("close connection")
+	sock.close ()
+
+
+def sendPassword (sock, password):
 	sock.send(password.encode())
 	answer = sock.recv(1024).decode()
 	if (answer =="CANCEL"):
 		sock.close ()
 		print ("close because of wrong password")
-		return
+		return None
 
-	#send the basefolder
+def sendBaseFolder (sock,root):
 	sock.send(os.path.basename(os.path.normpath(root)).encode())
 	sock.recv(1024)
-	#send the dictionary size
-	sock.send(str(len(str(fileDictionary).encode ())).encode())
+
+def sendDictionary (sock,fileDictionary):
+	dictionarySize = len(str(fileDictionary).encode ())
+	sock.send(str(dictionarySize).encode ())
 	sock.recv(1024)
-	#send the dictionary
 	sock.send(str(fileDictionary).encode ())
 
+def sendFiles (sock):
 	while True:
 		filename = sock.recv(1024).decode()
 		if (filename == "#end"):
@@ -69,9 +74,15 @@ def openConnection (host, port, root):
 				bytesToSend = f.read(1024)
 				sock.send(bytesToSend)
 
-
-	print ("close connection")
-	sock.close ()
+def getFileDictionary (root):
+	os.chdir(root)
+	fileDictionary = {}
+	for path, subdirs, files in os.walk("."):
+		for name in files:
+			fullFileName = os.path.join(path, name)
+			if os.path.isfile(fullFileName):
+				fileDictionary[fullFileName] = os.path.getsize(fullFileName)
+	return fileDictionary
 
 if __name__ == '__main__':
   Main()
